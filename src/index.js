4a9005511c2638bc5281b0179349c14f9524757a
GabrielGeron <144959369+GabrielGeron@users.noreply.github.com>
2024-06-05 02:29:29 +0000

    avances

---
+++ src/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    const action = ['ate', 'peed', 'crushed', 'broke'];
+    const what = ['my homework', 'my phone', 'the car'];
+    const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+    const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
+  
+    const generateExcuse = () => {
+      const excuse = `${getRandomElement(who)} ${getRandomElement(action)} ${getRandomElement(what)} ${getRandomElement(when)}`;
+      return excuse;
+    };
+  
+    const excuseElement = document.getElementById('excuse');
+    excuseElement.innerHTML = generateExcuse();
+  });
+  
