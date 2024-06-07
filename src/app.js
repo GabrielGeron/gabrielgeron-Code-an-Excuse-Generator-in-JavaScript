67ec6c5b3165b7aade1c891bcabb3c3e83b8e005
GabrielGeron <144959369+GabrielGeron@users.noreply.github.com>
2024-06-07 03:19:40 +0000

    cambios finales

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "my phone", "the car"];
+  const when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomElement = arr => {
+    if (!Array.isArray(arr) || arr.length === 0) {
+      throw new Error("Invalid array");
+    }
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  const generateExcuse = () => {
+    const excuse = `${getRandomElement(who)} ${getRandomElement(
+      action
+    )} ${getRandomElement(what)} ${getRandomElement(when)}`;
+    return excuse;
+  };
+
+  const excuseElement = document.getElementById("excuse");
+
+  // Verificar si es la primera carga
+  if (localStorage.getItem("firstLoad") === null) {
+    // Establecer la bandera de primera carga
+    localStorage.setItem("firstLoad", "false");
+  } else {
+    // Generar y mostrar una nueva excusa
+
+    excuseElement.innerHTML = generateExcuse();
+  }
 };
